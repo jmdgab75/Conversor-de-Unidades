a55d717129b96e32ad5196ca4c30983254ce19fa
Gabriel <gabriel.diniz@estudante.ifms.edu.br>
2023-07-12 13:19:23 -0400

    Commit Final

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,17 @@
-function coverterTemperatura() {
-    let celsius = parseFloat(document.getElementById("celsius").value);
-    let fahrenheit = (celsius * 9/5) + 32;
+function converterTemperatura() {
+    var celsius = parseFloat(document.getElementById("celsius").value);
+    var fahrenheit = (celsius * 9/5) + 32;
     document.getElementById("resultadoTemperatura").textContent = fahrenheit.toFixed(2) + " °F";
-}
+  }
 
-function coverterDistancia() {
-    let quilometros = parseFloat(document.getElementById("celsius").value);
-    let milhas = quilometros * 0,621371;
+  function converterDistancia() {
+    var quilometros = parseFloat(document.getElementById("quilometros").value);
+    var milhas = quilometros * 0.621371;
     document.getElementById("resultadoDistancia").textContent = milhas.toFixed(2) + " mi";
-}
+  }
 
-function coverterMassa() {
-    let quilograma = parseFloat(document.getElementById("quilograma").value);
-    let libras = quilograma * 2.20462;
-    document.getElementById("resultadoMassa").textContent = libras.toFixed(2) + " lb";
-}
-
+  function converterPeso() {
+    var quilogramas = parseFloat(document.getElementById("quilogramas").value);
+    var libras = quilogramas * 2.20462;
+    document.getElementById("resultadoPeso").textContent = libras.toFixed(2) + " lb";
+  }
